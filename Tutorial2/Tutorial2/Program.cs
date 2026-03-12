852aef1983cf30346eaa041cf7d9ffa718f5dbfe
Dawid Ochnio <s33682@pjwstk.edu.pl>
2026-03-12 12:46:45 +0100

    Class Test added

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+Test jeden = new Test("Dawid");
+
+
+
+class Test
+{
+    private string name;
+
+    public Test(string name)
+    {
+        this.name = name;
+    }
+
+
+}
